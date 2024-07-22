329a81758ec27833c3106574eb3dfc1a3922fac9
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-22 14:28:47 +0200

    feat: provide TypeOrmModule and DbModule in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { AppController } from './app.controller';
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { DbModule } from './db/db.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from './orm.config';
 
 @Module({
-  imports: [AuthModule, CartModule, OrderModule],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    DbModule.forRoot(),
+    AuthModule,
+    CartModule,
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
