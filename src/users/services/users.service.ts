64cec7a242fd980fb0564cb8ff397b074fbde64b
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-28 13:20:59 +0200

    fix: update UsersService to work with DB

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,27 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
-import { v4 } from 'uuid';
-
-import { User } from '../models';
+import { DbService } from '../../db/db.service';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private readonly users: Record<string, User>;
+  constructor(private readonly db: DbService) {}
 
-  constructor() {
-    this.users = {};
+  async findOne(userName: string, password: string): Promise<User> {
+    return this.db.users.findOneByNameAndPassword(userName, password);
   }
 
-  findOne(userId: string): User {
-    return this.users[userId];
-  }
-
-  createOne({ name, password }: User): User {
-    const id = v4();
-    const newUser = { id: name || id, name, password };
-
-    this.users[id] = newUser;
-
-    return newUser;
+  async createOne(name: string, password: string): Promise<User> {
+    return await this.db.users.create(name, password);
   }
 }
