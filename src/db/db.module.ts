77813ce041cae7ca12e700d7b0c9cd6e221d293c
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-28 13:24:39 +0200

    fix: provide UserDbService, OrderDbService,CartItemDbService to DbModule

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -6,6 +6,9 @@ import { Cart } from '../cart/entities/cart.entity';
 import { CartItem } from '../cart/entities/cart-item.entity';
 import { Order } from '../order/entities/order.entity';
 import { User } from '../users/entities/user.entity';
+import { UserDbService } from './db-services/user.db.service';
+import { OrderDbService } from './db-services/order.db.service';
+import { CartItemDbService } from './db-services/cart-item.db.service';
 
 @Global()
 @Module({
@@ -17,7 +20,13 @@ export class DbModule {
     return {
       module: DbModule,
       imports: [TypeOrmModule.forFeature([Cart, CartItem, Order, User])],
-      providers: [DbService, CartDbService],
+      providers: [
+        DbService,
+        CartDbService,
+        UserDbService,
+        OrderDbService,
+        CartItemDbService,
+      ],
       exports: [DbService],
     };
   }
